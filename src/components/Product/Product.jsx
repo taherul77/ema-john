ddd6f2f665dc74b30d5420688f262438757ddb63
taherul77 <taherul2317@gmail.com>
2023-03-30 16:15:09 +0600

    product image ok

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const {img , name, price, seller, quantity} =props.product;
+    const {img , name, price, seller, ratings, quantity} =props.product;
     return (
         <div className='product'>
            <img src={img} alt="" />
+           <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price:${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings}</p>
+                <p>Quantity: {quantity}</p>
+           </div>
+           <button className='btn-card'>Add to Card</button>
         </div>
     );
 };
